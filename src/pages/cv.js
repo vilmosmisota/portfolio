fe6a4c45917553082cb645e956c0938162d81e84
Vilmos Misota <misotavilmos@gmail.com>
2021-07-14 08:16:42 +0100

    fix bug

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -2,7 +2,7 @@ import { StaticImage } from "gatsby-plugin-image"
 import React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/Seo"
-import "../assets/styles/CV.css"
+import "../assets/styles/cv.css"
 
 function cv() {
   return (
